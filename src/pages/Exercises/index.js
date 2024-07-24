2c1ee52cfc70131cf2eb71e08582a111ce708c2b
git config --global user.email joaopr0701@gmail.com <joaopr0701@gmail.com>
2024-07-24 11:10:52 -0300

    tela de exercicios atualizada

--- src/pages/Exercises/index.js
+++ src/pages/Exercises/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, increaseDistance, decreaseDistance } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 
 export default function Exercises() {
@@ -28,123 +30,161 @@ export default function Exercises() {
     ); 
   };
 
-  const distance = () => {
-    const [distance, setDistance] = useState(0); // Inicializa a distância como 0 km
-  
-    const increaseDistance = () => {
-      // Aumenta a distância em 200 metros (0.2 km)
-      setDistance(prevDistance => prevDistance + 0.2);
-    };
-  
-    const decreaseDistance = () => {
-      // Diminui a distância em 200 metros (0.2 km), mas não abaixo de 0 km
-      if (distance >= 0.2) {
-        setDistance(prevDistance => prevDistance - 0.2);
-      }
-    };
+  const [distance, setDistance] = useState(0.00);
+
+  const increaseDistance = () => {
+    // Aumenta a distância em 200 metros (0.2 km)
+    setDistance(prevDistance => prevDistance + 0.4)
+  };
+
+  const decreaseDistance = () => {
+    // Diminui a distância em 200 metros (0.1 km), mas não abaixo de 0 km
+    if (distance >=  0.4) {
+      setDistance(prevDistance => prevDistance - 0.4);
+    }
   };
   
+  
   return (
-    <View style={styles.container}>
-    <Text style={styles.titulo}> Exercícios e Atividade Física </Text>
-    <Text style={styles.subtitulo}>Cronometre seu tempo praticando e veja seu desemprenho total.
-    </Text>
+  <View style={styles.container}>
+  <Text style={styles.titulo}> Exercícios e Atividade Física </Text>
+  <Text style={styles.subtitulo}>Cronometre seu tempo praticando e veja seu desemprenho total.
+  </Text>
        
-    <TouchableOpacity
-    style={styles.botao}
-    onPress={() => console.log('Iniciar exercício')}>
-    <Text style={styles.buttonText}>Iniciar</Text>
-    </TouchableOpacity>
+  <View style={styles.buttonprincipal}>  
+  <TouchableOpacity
+  style={styles.button}
+  onPress={() => console.log('Iniciar exercício')}>
+  <Text style={styles.buttonText}>Iniciar</Text>
+  </TouchableOpacity> 
+  </View>
+  <View style={styles.kmcontainer}>
+  <View style={styles.kmtext}>
+  <Text>{distance.toFixed(2)} km</Text>
+  </View>
+  </View>
 
-    <View>
-      <TouchableOpacity 
-      style={styles.button1}
-       onPress={() =>increaseDistance}>
-        <Text style={styles.buttonText1}> + </Text>
-      </TouchableOpacity>
+  <View style={styles.buttoncontainer}>
+  <TouchableOpacity
+  style={styles.button1}
+  onPress={() => increaseDistance()}>
+  <Text style={styles.buttonText1}> + </Text>
+  </TouchableOpacity>
 
-      <TouchableOpacity 
-      style={styles.button2}
-      onPress={() =>decreaseDistance}>
-        <Text style={styles.buttonText2}> - </Text>
-      </TouchableOpacity>
-      </View>
-
-
-    </View>
+  <TouchableOpacity
+  style={styles.button2}
+  onPress={() => decreaseDistance()}>
+  <Text style={styles.buttonText2}> - </Text>
+  </TouchableOpacity>
+  </View>
+  </View>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      flexDirection: 'column',
-      marginTop: 0,
-      borderTopLeftRadius: 0,
-      borderTopRightRadius: 0,
-      backgroundColor: '#ffffff',
-      justifyContent: 'space-between',
-    },
-    titulo: {
-      fontWeight: 'bold',
-      fontSize: 40,
-      marginTop: 70,
-      paddingHorizontal: 10,
-      marginHorizontal: 20,
-      marginBottom: 20,
-      textAlign:   'left',
-    },
-    subtitulo: {
-      fontSize: 16,
-      paddingHorizontal: 10,
-      marginHorizontal: 17,
-      marginBottom: 20,
-      marginTop: -70,
-    },
-    botao: {
-      backgroundColor: '#7DCD9A',
-      paddingVertical: 10,
-      paddingHorizontal: 0,
-      borderRadius: 15,
-      height: 64,
-      width: 150,
-      alignItems: 'center',
-      alignSelf: 'center', 
-      margin: 40,
-      marginTop: 300,
-    },
-    buttonText : {
-        backgroundColor: '#7DCD9A',
-        paddingVertical: 10,
-        paddingHorizontal: 0,
-        borderRadius: 15,
-        height: 64,
-        width: 150,
-        alignItems: 'center',
-        alignSelf: 'center', 
-        margin: 40,
-        marginTop: 300,
-    },
-    buttonText1:{
-        margin: 40,
-        marginTop: 300,
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    flexDirection: 'column',
+    marginTop: 0,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 0,
+    backgroundColor: '#ffffff',
+    justifyContent: 'space-between',
+  },
+  titulo: {
+    fontWeight: 'bold',
+    fontSize: 42,
+    marginTop: 70,
+    paddingHorizontal: 10,
+    marginHorizontal: 20,
+    marginBottom: 35,
+    left: -20,
+  },
+  subtitulo: {
+    color: 'black',
+    fontSize: 14,
+    paddingHorizontal: 10,
+    marginHorizontal: 17,
+    marginTop: -90,
+    left: -15,
+  },
+  buttonprincipal: {
+    flexDirection: 'column',
+    alignItems: 'center',
 
-    },
-    button2: {
-        backgroundColor: '#7DCD9A',
-        paddingVertical: 10,
-        paddingHorizontal: 0,
-        borderRadius: 15,
-        height: 64,
-        width: 150,
-        alignItems: 'center',
-        alignSelf: 'center', 
-        margin: 30,
-        marginTop: 300, 
-    },
-    buttonText2: {
-        margin: 40,
-        marginTop: 40,
-    },
-  });
+  },
+  button: {
+    backgroundColor: '#7DCD9A',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    height: 64,
+    width: 150,
+    alignItems: 'center',
+    top: 440,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+  buttoncontainer: {
+    flexDirection: 'row',
+    marginBottom: 100,
+    bottom: 70,
+    alignSelf: 'center',
+    position: 'relative',
+  },
+  button1: {
+    backgroundColor: '#D9D9D9',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    height: 60,
+    width: 'auto',
+    left: -90,
+    right: 20,
+  },
+  buttonText1: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+    left: 0,
+  },
+  button2: {
+    backgroundColor: '#D9D9D9',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    width: 'auto',
+    left: 90,
+    height: 60,
+    right: 20,
+  },
+  buttonText2: {
+    color: 'black',
+    fontSize: 30,
+    fontWeight: 'bold',
+    left: 0,
+  },
+  kmcontainer: {
+    flexDirection: 'row',
+    bottom: -40,
+    alignSelf: 'center',
+    position: 'relative',
+  },
+  kmtext: {
+    fontSize: 70,
+    color: 'black',
+    paddingHorizontal: 10,
+    marginHorizontal: 17,
+    marginBottom: 20,
+    marginTop: 150,
+    alignItems: 'center',
+
+   },
+});
+
+
 
